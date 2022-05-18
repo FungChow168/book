776cf75fae2b2a6fbd5df910c2f5515e77871ff4
cliveford <cliveford2020@gmail.com>
2022-05-18 16:50:25 +0100

    second commit

---
+++ src/test/java/com/cliveford/book/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliveford.book.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
